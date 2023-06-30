2a101415a308175ed9d5d486aaa5633160af03d9
nlprasanna5 <lakshmi.76740@gmail.com>
2023-06-30 12:26:51 +0530

    modified dropdown styling for mobile and tablet view

--- src/components/aboutDropDown/AboutDropDown.js
+++ src/components/aboutDropDown/AboutDropDown.js
@@ -5,20 +5,42 @@ function AboutDropDown() {
   return (
     <div className='main-container'>
 
-    <div className='first'>
+      <div className='first'>
+
         <p className='headings'>Heading</p>
-        <p className='line-para'>About Item 1</p>
-       <p className='line-para'>About Item 2</p>
-       <p className='line-para'>About Item 3</p>
-       <p className='line-para'>About Item 4</p>
-    </div>
-    <div className='second'>
-    <p className='headings'>Heading</p>
-    <p className='line-para'>About Item 1</p>
-       <p className='line-para'>About Item 2</p>
-       <p className='line-para'>About Item 3</p>
-       <p className='line-para'>About Item 4</p>
-    </div>
+        <div className='line-para'>
+          <p >About Item 1</p>
+        </div>
+        <div className='line-para'>
+          <p >About Item 2</p>
+        </div>
+        <div className='line-para'>
+          <p >About Item 3</p>
+        </div>
+        <div className='line-para'>
+          <p >About Item 4</p>
+        </div>
+
+
+      </div>
+      <div className='second'>
+        <p className='headings'>Heading</p>
+        <div className='line-para'>
+          <p >About Item 1</p>
+        </div>
+        <div className='line-para'>
+          <p >About Item 2</p>
+        </div>
+        <div className='line-para'>
+          <p >About Item 3</p>
+        </div>
+        <div className='line-para'>
+          <p >About Item 4</p>
+        </div>
+
+
+
+      </div>
     </div>
   )
 }
